f9df6afdc0880b40b31258c6d0c6ea82849619cb
Paula Pereyra <paulapereyrallorens@gmail.com>
2021-04-15 11:57:47 -0300

    Relacion 1-1 tabla users y profile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        Moto::factory(10)->create();
-        $this->call(AutoSeeder::class);
+        \App\Models\User::factory(10)->create();
+        //Moto::factory(10)->create();
+        //$this->call(AutoSeeder::class);
         
     }
 }
